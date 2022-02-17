e2ac2b968660fe01b043b4954b5bc197aa2eecb5
TrevorHsu07 <thsu4@gmu.edu>
2022-02-16 21:28:40 -0500

    Public variables self set

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -6,11 +6,17 @@ using UnityEngine.SceneManagement;
 public class MenuManager : MonoBehaviour
 {
 
+    //Sets the separate menu variables
     public Canvas TitleUI;
     public Canvas OptionsUI;
 
     void Start()
     {
+        //Manually sets the two public menu variables
+        TitleUI = GameObject.Find("TitleScreenCanvas").GetComponent<Canvas>();
+        OptionsUI = GameObject.Find("OptionsCanvas").GetComponent<Canvas>();
+
+        //Starts with the title screen enabled and the options screen false
         TitleUI.enabled = true;
         OptionsUI.enabled = false;
     }
@@ -22,12 +28,14 @@ public class MenuManager : MonoBehaviour
         Debug.Log("QUIT");
     }
 
+    //Function for entering the options menu
     public void OptionSelect()
     {
         TitleUI.enabled = false;
         OptionsUI.enabled = true;
     }
 
+    //Function for exiting the option menu back to title screen
     public void OptionsBack()
     {
         TitleUI.enabled = true;
