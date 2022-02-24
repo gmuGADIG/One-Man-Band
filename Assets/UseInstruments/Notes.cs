a52093bdfd078e3be15511225bb9d6c381b2430b
Aiden Young <ayoung49@gmu.edu>
2022-02-23 21:32:15 -0500

    Note script is changed to move toward the mouse and not stop at the mouse. As well as added three tags BlueNote, RedNote, and GreenNote.

--- Assets/UseInstruments/Notes.cs
+++ Assets/UseInstruments/Notes.cs
@@ -8,6 +8,7 @@ public class Notes : MonoBehaviour
     public bool red, green, blue;
     public float moveSpeed;
     private Vector3 cursorPosition;
+    private Vector2 forceVelocity = new Vector2(0.0f, 0.0f);
     public GameObject note;
 
 
@@ -19,15 +20,45 @@ public class Notes : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+
+
         Move();
+        if (Input.GetMouseButtonDown(0))
+        {
+            cursorPosition = Input.mousePosition;
+            cursorPosition = Camera.main.ScreenToWorldPoint(cursorPosition);
+            cursorPosition -= note.transform.position;
+            setVelocity(cursorPosition, "Red");
+        }
     }
 
+    public void setVelocity(Vector2 vel, string color)
+    {
+        forceVelocity = vel;
+        if (color.Equals("Blue"))
+        {
+            red = false;
+            blue = true;
+            green = false;
+        }
+        else if (color.Equals("Red"))
+        {
+            red = true;
+            blue = false;
+            green = false;
+        }
+        else if (color.Equals("Green"))
+        {
+            red = false;
+            blue = false;
+            green = true;
+        }
+    }
     
     void Move()
     {
-        cursorPosition = Input.mousePosition;
-        cursorPosition = Camera.main.ScreenToWorldPoint(cursorPosition);
-        note.transform.position = ((cursorPosition) / moveSpeed);
+        note.transform.position += (Vector3.Normalize(forceVelocity) * moveSpeed) * Time.deltaTime;
         note.transform.position = new Vector3(note.transform.position.x, note.transform.position.y, 0.0f);
     }
 }
