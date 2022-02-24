e9640cc43d974708d0c7823b8d1f89daded68ce8
Stephen H <shapp@masonlive.gmu.edu>
2022-02-23 21:33:55 -0500

    Updated EnemySpawner prefab and script

--- Assets/EnemySpawnStuff/EnemySpawnerScript.cs
+++ Assets/EnemySpawnStuff/EnemySpawnerScript.cs
@@ -5,15 +5,24 @@ using UnityEngine;
 public class EnemySpawnerScript : MonoBehaviour
 {
     [Tooltip("Original enemy (or any object) for this spawner to spawn")]
-    public Object enemyType;
-    [Tooltip("Number of enemies spawned at a time")]
+    public GameObject enemyType;
+    [Tooltip("Number of enemies spawned at a time, but may spawn less if spawning this many would produce more enemies in the level than the int Max Enemies")]
     public uint spawnSize;
+    [Tooltip("Only spawns enemies if less than this number of enemies are alive in the level")]
+    public uint maxEnemies;
     [Tooltip("Spawner will wait at least this many seconds between spawning each enemy")]
     public float spawnIntervalLowerBound;
     [Tooltip("Spawner will wait at most this many seconds between spawning each enemy")]
     public float spawnIntervalUpperBound;
     [Tooltip("Spawns enemies only while this is true/checked")]
     public bool spawnCheck;
+    [Tooltip("The player object in this scene, the spawner only spawns enemies if the player is close enough to it")]
+    public GameObject player;
+    [Tooltip("Only spawns enemies if player is within this distance of spawner")]
+    public float maxSpawningDistance;
+
+    public static uint enemiesAliveInLevel; //Should be set to 0 by the scene when the level starts, 
+                                            //decremented by enemies as they despawn
 
     private float nextSpawn = 0.0f;
 
@@ -21,6 +30,11 @@ public class EnemySpawnerScript : MonoBehaviour
     void Start()
     {
         incrementNextSpawn();
+        //Below if statement sets the count of alive enemies to 0 if it's currently the start of the scene
+        if (Time.frameCount <= 1)
+        {
+            enemiesAliveInLevel = 0;
+        }
     }
 
     // Update is called once per frame
@@ -28,13 +42,21 @@ public class EnemySpawnerScript : MonoBehaviour
     {
         if (Time.time > nextSpawn)
         {
-            //spawnCheck is separated from nextSpawn to allow random increments to continue
-            //(prevents spawner(s) from immediately spawning once spawnCheck is set to true)
-            if (spawnCheck)
+            bool playerIsClose = Vector3.Distance(player.transform.position, gameObject.transform.position) < maxSpawningDistance;
+            //spawnCheck and playerIsClose are separated from nextSpawn to allow random increments to continue
+            //(prevents spawner(s) from immediately spawning once spawnCheck is set to true/player gets close enough)
+            if (spawnCheck && playerIsClose)
             {
-                for (uint i = 0; i < spawnSize; ++i)
+                uint amountToSpawn = spawnSize; //Number of enemies to spawn this time
+                if (spawnSize + enemiesAliveInLevel > maxEnemies) //If spawning spawnSize enemies isn't allowed by maxEnemies
+                {
+                    amountToSpawn = maxEnemies - enemiesAliveInLevel; //Only spawn enough enemies to reach maxEnemies
+                }
+                for (uint i = 0; i < amountToSpawn; ++i)
                 {
                     Object.Instantiate(enemyType, gameObject.transform.position, gameObject.transform.rotation);
+                    enemiesAliveInLevel++;
+
                 }
             }
             incrementNextSpawn();
