658fbec2de351d83b1c8ec456d7cdc506c5c7d88
Dheer Tammina <dtammina@gmail.com>
2022-03-23 21:31:29 -0400

    added new script for violinSpider

---
+++ Assets/Scripts/ViolinSpider.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViolinSpider : MonoBehaviour
+{
+    //variables
+    public GameObject player;
+    public Animator anim;
+    public int health; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(){
+            attack();
+        }
+        else if(){  
+            death();
+        }
+        else{
+            walk();
+        }
+    }
+
+    void walk(){
+
+    }
+
+    void attack(){
+
+    }
+
+    void death(){
+
+    }
+}
