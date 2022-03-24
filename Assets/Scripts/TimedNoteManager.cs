807dbf8d1b4a06355eff2941837c6276b6a63194
OmarNasir <onasir2.gmu.edu>
2022-03-23 21:20:03 -0400

    New notes, for each specfic note, and it kinda works

--- Assets/Scripts/TimedNoteManager.cs
+++ Assets/Scripts/TimedNoteManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class TimedNoteManager : MonoBehaviour
 {
-    public bool check = false;
+
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/Scripts/TimedNotes.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedNotes : ParentNote
+{
+    DaddyNote Dn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Dn = transform.parent.GetComponent<DaddyNote>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Dn.StartTracking();
+            gameObject.SetActive(false);
+        }
+    }
+}
