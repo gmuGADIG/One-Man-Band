c7a107d211911cb94cd87e493921ff5af5d4421f
Lam Phan <lphan21@gmu.edu>
2022-03-30 21:32:53 -0400

    Pause function works

--- Assets/Scenes/UIPauseChamp/PauseChamp.cs
+++ Assets/Scenes/UIPauseChamp/PauseChamp.cs
@@ -6,10 +6,35 @@ using UnityEngine.SceneManagement;
 
 public class PauseChamp : MonoBehaviour
 {
+    public GameObject pauseScreen;
+    public int counter = 0;
+
+    void Start() {
+        pauseScreen.SetActive(false);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            pauseScreen.SetActive(true);
+            counter += 1;
+            Pause();
+        }
+        else if (counter == 2)
+        {
+            counter = 0;
+            pauseScreen.SetActive(false);
+            Resume();
+        }
     }
-        
+
+    void Pause (){
+        Time.timeScale = 0;
+    }
+    
+    void Resume (){
+        Time.timeScale = 1;
+    }
+
 }
