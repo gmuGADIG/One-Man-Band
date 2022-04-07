3b46d62c871a6cc08cd48db616de4785a52b4cd3
Nicholas Szymonik <nszymoni@gmu.edu>
2022-04-06 21:42:16 -0400

    Chest Explosion!

--- Assets/NoteChest.cs
+++ Assets/NoteChest.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class NoteChest : MonoBehaviour
 {
-    private IEnumerable disperseCoroutine;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,14 +22,36 @@ public class NoteChest : MonoBehaviour
     {
         if(other.gameObject.GetComponent<Notes>() != null)
         {
+            GetComponent<SpriteRenderer>().enabled = false;
+            GetComponent<Collider2D>().enabled = false;
+            StartCoroutine(disperse());
+        }
+    }
+
+    IEnumerator disperse()
+    {
+        float[] radius = new float[transform.childCount];
+        float[] angle = new float[transform.childCount];
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(true);
+            radius[i] = Random.Range(0.01f, 0.05f);
+            angle[i] = Random.Range(-20f, 20f); 
+        }
+        float timer = Time.time; 
+        while (Time.time - timer < 0.25f)
+        {
             for (int i = 0; i < transform.childCount; i++)
             {
-                GameObject drop = transform.GetChild(i).gameObject;
-                drop.SetActive(true);
-                //StartCoroutine(disperse());
+                transform.GetChild(i).Translate(
+                    radius[i] * Mathf.Cos(angle[i] + 360 * i / transform.childCount), 
+                    radius[i] * Mathf.Sin(angle[i] + 360 * i / transform.childCount), 
+                    0);
+
             }
-            transform.DetachChildren();
-            Destroy(gameObject);
+            yield return null;
         }
+        transform.DetachChildren();
+        Destroy(gameObject);
     }
 }
