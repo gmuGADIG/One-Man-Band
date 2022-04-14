8593716d05f239f26a6340e7ec7aefc995a6cbdf
Nicholas Szymonik <nicholascoles.a@gmail.com>
2022-04-13 20:15:22 -0400

    refactoring orc script

--- Assets/Scripts/Orc.cs
+++ Assets/Scripts/Orc.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Orc : MonoBehaviour
+public class Orc : BaseEnemy
 {
     public Transform player;
     public float movespeed = 2f;
@@ -12,20 +12,17 @@ public class Orc : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        base.Start();
         rb = GetComponent<Rigidbody2D>();
-		player = GameObject.FindGameObjectWithTag("Player").transform; 
 		// in the future try not to push stuff with errors pl0x&th0x :) - David
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 direction = player.position - transform.position;
-        Debug.Log(direction);
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg; // i assume your IDE isnt doing autocomplete so maybe do some googling on how to fix that lol - David
-        rb.rotation = angle;
-        direction.Normalize();
-        movement = direction;
+        Vector3 direction = Target.position - transform.position;
+        rb.rotation = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        movement = direction.normalized;
     }
     // Update enemy movement following convert to Vector2
     // Note: Add acceleration
