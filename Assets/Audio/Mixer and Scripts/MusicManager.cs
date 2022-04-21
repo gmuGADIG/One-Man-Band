0cf6a6df41e603a3aa43b9a610311aa8f8f05cb3
Duke-of-Disastr <102875676+Duke-of-Disastr@users.noreply.github.com>
2022-04-20 21:15:28 -0400

    Added my helpful Functions

--- Assets/Audio/Mixer and Scripts/MusicManager.cs
+++ Assets/Audio/Mixer and Scripts/MusicManager.cs
@@ -8,11 +8,13 @@ public class MusicManager : MonoBehaviour
 
     [HideInInspector] public AudioSource[] sources;
 
+
     private void Awake()
     {
+        //This audio source will not be destoyed between scene loads
         DontDestroyOnLoad(gameObject);
-        sources = new AudioSource[tracks.Count];
-        for (int i = 0; i < tracks.Count; i++)
+        sources = new AudioSource[tracks.Count]; //init array
+        for (int i = 0; i < tracks.Count; i++) //assigns array and track properties
         {
             sources[i] = gameObject.AddComponent<AudioSource>();
             sources[i].clip = tracks[i];
@@ -26,6 +28,28 @@ public class MusicManager : MonoBehaviour
     public void SetTrackVolume(int _track, float _volume = 1)
     {
         sources[_track].volume = _volume;
+    }
+    //basically couroutine in a function that fades
+    public void FadeInTrack(int _track, float _targetVolume = 1, float overTime = 1)
+    {
+        IEnumerator Fade()
+        {
+            AudioSource targetTrack = sources[_track];
+            float startingVolume = targetTrack.volume;
+            float t = 0;
+            
+            while(targetTrack.volume < _targetVolume)
+            {
+                targetTrack.volume = Mathf.Lerp(startingVolume, _targetVolume, t);
+                if (t < 1)
+                {
+                    t += Time.deltaTime / overTime;
+                }
+                yield return null;
+            }
+            yield return null;
+        }
+        StartCoroutine(Fade());
     }
     public void TurnOffMusic()
     {
---
+++ Assets/Scripts/ClaytonsHelpfulFunctions.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClaytonsHelpfulFunctions : MonoBehaviour
+{
+    public static float CutDecimal(float number)
+    {
+        float roundedNumber = Mathf.Round(number);
+        if (roundedNumber > number)
+        {
+            float z = roundedNumber - number;
+            float a = 1 - z;
+            return number - a;
+        }
+        else
+        {
+            return roundedNumber;
+        }
+    }
+    public static float CutDecimal(float number, out float decimalReturn)
+    {
+        float roundedNumber = Mathf.Round(number);
+
+        if (roundedNumber > number)
+        {
+            float difference = roundedNumber - number;
+            float decimalNumber = 1 - difference;
+            decimalReturn = decimalNumber;
+            return number - decimalNumber;
+        }
+        else
+        {
+            float difference = number - roundedNumber;
+            decimalReturn = difference;
+            return roundedNumber;
+        }
+    }
+    public static void Teleport(Transform a, Transform b)
+    {
+        a.position = b.position;
+    }
+
+    public static bool ExclusiveOr(bool a, bool b)
+    {
+        return ((a && !b) || (!a && b));
+    }
+    public static T ReturnRandomElement<T>(T[] list)
+    {
+        return list[Mathf.RoundToInt(Random.Range(0, list.Length - 1))];
+    }
+}
